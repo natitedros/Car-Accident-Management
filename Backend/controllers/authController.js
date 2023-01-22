1696191ac9153efe2e23d7e94ef46ca8e6ff7d41
Nati_Tedros <tedros.natnael@gmail.com>
2023-01-22 16:51:07 +0300

    login added

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -42,3 +42,20 @@ module.exports.signup_post = async (req, res)=>{
         // console.log(err)
     }
 }
+
+module.exports.login_post = async (req, res)=>{
+    const {email, password} = req.body
+
+    try{
+        const user = await User.login(email, password)
+        const token = createToken(user._id)
+
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+        res.status(200).json({ user: user._id})
+    }catch(err){
+
+        const errors = handleErrors(err)
+        res.status(400).json({ errors })
+        
+    }
+}
