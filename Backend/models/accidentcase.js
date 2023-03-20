aebda0a8e02c8e9706987d2acff4ed48b958bf7c
Nati_Tedros <tedros.natnael@gmail.com>
2023-03-20 19:54:52 +0300

    Updated cars and cases model

--- Backend/models/accidentcase.js
+++ Backend/models/accidentcase.js
@@ -21,7 +21,28 @@ const caseSchema = new mongoose.Schema({
     },
     handlerId: {
         type: String
+    },
+    severity: {
+        type: String,
+        enum: ["minor", "major"]
+    },
+    carColor: {
+        type: String,
+        required: true
+    },
+    carPlateNumber: {
+        type: String,
+        required: true
+    },
+    carName: {
+        type: String,
+        required: true
+    },
+    carModel: {
+        type: String,
+        required: true
     }
+
 
 },{timestamps: true})
 const Case = mongoose.model('accidentcase', caseSchema)
