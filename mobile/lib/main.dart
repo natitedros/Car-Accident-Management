694c28ca4dcd89d4e2d6fc8c87e2e3281669401b
natitedros <tedros.natnael@gmail.com>
2023-05-13 18:35:25 +0300

    added local notification

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,14 +1,24 @@
-//import 'dart:ui';
 
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
-
 import 'package:car_accident_management/pages/login.dart';
 import 'package:car_accident_management/pages/signup.dart';
-import 'package:car_accident_management/pages/driver/driver.dart';
-import 'package:car_accident_management/pages/admin/admin.dart';
-import 'package:car_accident_management/pages/police/police.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  AwesomeNotifications().initialize(
+      'resource://drawable/notification_logo',
+      [            // notification icon
+        NotificationChannel(
+        channelGroupKey: 'basic_test',
+        channelKey: 'basic',
+        channelName: 'Basic notifications',
+            channelDescription: 'Notification channel for basic tests',
+            channelShowBadge: true,
+            importance: NotificationImportance.High,
+            enableVibration: true,
+      )]
+  );
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
